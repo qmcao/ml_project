eeceee76490a7bdc327f543a607c736b2c73a890
Cao, Michael <minh.caoq01@gmail.com>
2024-11-22 13:28:17 -0800

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys 
+import logging
+def error_merssage_detail(error, error_detal:sys):
+    _,_,exc_tb = error_detal.exc_info()
+    file_name= exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+        file_name,
+        exc_tb.tb_lineno,
+        str(error) 
+    )
+    return error_message
+
+class CustomeException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_merssage_detail(error_message, error_detal=error_detail)
+        
+    def __str__(self):
+        return self.error_message
+    
+
+if __name__ == "__main__":
+    try:
+        a= 1/0
+    except Exception as e:
+        logging.info("Divide by zero")
+        raise CustomeException(e, sys)
